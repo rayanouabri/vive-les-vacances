6b514418465d307b7cdb2a5aa32a2527adb83c75
rayan ouabri <rayan.ouabri1@gmail.com>
2026-03-16 16:18:05 +0100

    Configure Firebase forum + code publication

--- forum-config.js
+++ forum-config.js
@@ -4,16 +4,16 @@
 window.VLV_FORUM_CONFIG = {
     // Code requis pour publier un nouveau sujet.
     // Les reponses restent ouvertes a tous.
-    postCreationCode: "CHANGE_ME",
+    postCreationCode: "Six-Seven",
 
     // Configuration Firebase (Firestore) pour activer le forum temps reel multi-utilisateurs.
     // Si tu laisses les valeurs de demo, le forum reste en mode local (localStorage).
     firebase: {
-        apiKey: "REMPLACE_PAR_TA_CLE_API",
-        authDomain: "REMPLACE.firebaseapp.com",
-        projectId: "REMPLACE_PAR_TON_PROJECT_ID",
-        storageBucket: "REMPLACE.appspot.com",
-        messagingSenderId: "123456789",
-        appId: "1:123456789:web:abcdef"
+        apiKey: "AIzaSyDkapn8chCT8ghL69hn7xbaY9S_RIE_x0s",
+        authDomain: "vlv-forum.firebaseapp.com",
+        projectId: "vlv-forum",
+        storageBucket: "vlv-forum.firebasestorage.app",
+        messagingSenderId: "373024377238",
+        appId: "1:373024377238:web:f5924862b3745fa3a7f5b7"
     }
 };
